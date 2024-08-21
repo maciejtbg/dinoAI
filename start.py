5f60024dc355399bd6bcae3a27cc281429aef60d
Maciej Wyrzykowski <m.wyr@o2.pl>
2024-08-21 17:17:36 +0200

    Ready to push to github.

--- start.py
+++ start.py
@@ -5,8 +5,8 @@ import cv2
 import numpy as np
 import pytesseract
 from matplotlib import pyplot as plt
-from gym import Env
-from gym.spaces import Box, Discrete
+from gymnasium import Env  # Zmienione na gymnasium
+from gymnasium.spaces import Box, Discrete
 
 class WebGame(Env):
     def __init__(self):
@@ -17,32 +17,61 @@ class WebGame(Env):
         self.game_location = {'top':150, 'left':130, 'width':250, 'height': 150}
         self.done_location = {'top':190, 'left':220, 'width':120, 'height':30}
 
+    # def step(self, action):
+    #     action_map = {
+    #         0:'space',
+    #         1:'down',
+    #         2:'no_op'
+    #     }
+    #     if action !=2:
+    #         pydirectinput.press(action_map[action])
+
+    #     done, done_cap = self.get_done()
+    #     new_observation = self.get_observation()
+    #     reward = 1
+    #     info = {}
+    #     return new_observation, reward, done, info
+
     def step(self, action):
         action_map = {
-            0:'space',
-            1:'down',
-            2:'no_op'
+            0: 'space',
+            1: 'down',
+            2: 'no_op'
         }
-        if action !=2:
+        if action != 2:
             pydirectinput.press(action_map[action])
 
         done, done_cap = self.get_done()
         new_observation = self.get_observation()
         reward = 1
         info = {}
-        return new_observation, reward, done, info
+
+        # Zakładamy, że w Twojej grze `done` oznacza zakończenie, czyli `terminated`.
+        terminated = done  
+        truncated = False  # Możesz ustawić `truncated` na `True`, jeśli masz warunki zakończenia związane z czasem.
+
+        return new_observation, reward, terminated, truncated, info
     
     def render(self):
         cv2.imshow('Game', np.array(self.cap.grab(self.game_location))[:,:,:3])
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
             self.close()
 
 
-    def reset(self):
+    # def reset(self):
+    #     time.sleep(1)
+    #     pydirectinput.click(x=150, y=150)
+    #     pydirectinput.press('space')
+    #     return self.get_observation()
+    
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)  # Ustawienie seed, jeśli jest podane
         time.sleep(1)
         pydirectinput.click(x=150, y=150)
         pydirectinput.press('space')
-        return self.get_observation()
+        observation = self.get_observation()
+        info = {}  # Możesz dodać tu dodatkowe informacje, jeśli potrzebujesz
+        return observation, info
     
     def close(self):
         cv2.destroyAllWindows()
@@ -54,13 +83,21 @@ class WebGame(Env):
     #     channel = np.reshape(resized, (1,83  ,100))
     #     return channel  
 
-    def get_observation(self):
-        raw = np.array(self.cap.grab(self.game_location))[:,:,:3]
-        gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
-        # resized= cv2.resize(gray, (150, 50))
-        # channel = np.reshape(resized, (1,50,150))
-        return gray
+    # def get_observation(self):
+    #     raw = np.array(self.cap.grab(self.game_location))[:,:,:3]
+    #     gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+    #     # resized= cv2.resize(gray, (150, 50))
+    #     # channel = np.reshape(resized, (1,50,150))
+    #     return gray
     
+    def get_observation(self):
+        raw = np.array(self.cap.grab(self.game_location))[:, :, :3]  # Pobranie obrazu o rozmiarze (150, 250)
+        gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)  # Konwersja na obraz w skali szarości
+        resized = cv2.resize(gray, (100, 83))  # Skalowanie do wymiarów (100, 83)
+        observation = np.expand_dims(resized, axis=0)  # Dodanie wymiaru kanału, aby uzyskać kształt (1, 83, 100)
+        return observation
+
+
     def get_done(self):
         # done_cap = np.array(self.cap.grab(self.done_location))[:,:,3]
         done_cap = np.array(self.cap.grab(self.done_location))
@@ -95,6 +132,22 @@ import os
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common import env_checker
 env_checker.check_env(env)
+# class TrainAndLoggingCallback(BaseCallback):
+#     def __init__(self, check_freq, save_path, verbose=1):
+#         super(TrainAndLoggingCallback, self).__init__(verbose)
+#         self.check_freq = check_freq
+#         self.save_path = save_path
+    
+#     def _init_callback(self):
+#         if self.save_path is not None:
+#             os.makedirs(self.save_path, exist_ok=True)
+    
+#     def _on_step(self):
+#         if self.n_calls % self.check_freq == 0:
+#             model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_class))
+#             self.model_save(model_path)
+#         return True
+    
 class TrainAndLoggingCallback(BaseCallback):
     def __init__(self, check_freq, save_path, verbose=1):
         super(TrainAndLoggingCallback, self).__init__(verbose)
@@ -107,31 +160,31 @@ class TrainAndLoggingCallback(BaseCallback):
     
     def _on_step(self):
         if self.n_calls % self.check_freq == 0:
-            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_class))
-            self.model_save(model_path)
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
         return True
-    
+
 CHECKPOINT_DIR = "./train/"
 LOG_DIR = './logs/'
 
 callback = TrainAndLoggingCallback(check_freq=300, save_path=CHECKPOINT_DIR)
 
 from stable_baselines3 import DQN
-model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=300000, learning_starts=1000)
+model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=100000, learning_starts=1000)
 
 model.learn(total_timesteps=1000, callback=callback)
 
 
-for episode in range(1): 
-    print(f'Episode: {episode}.')
-    obs = env.reset()
-    done = False
-    total_reward = 0 
+# for episode in range(1): 
+#     print(f'Episode: {episode}.')
+#     obs = env.reset()
+#     done = False
+#     total_reward = 0 
 
-    while not done:
-        action, _ =model.predict(obs)
-        obs, reward, done, info = env.step(int(action))
-        # obs,reward, done, info = env.step(env.action_space.sample())
-        total_reward +=reward
-    print(f'Total Reward for episode {episode} is {total_reward}')
-    time.sleep(2)
+#     while not done:
+#         action, _ =model.predict(obs)
+#         obs, reward, done, info = env.step(int(action))
+#         # obs,reward, done, info = env.step(env.action_space.sample())
+#         total_reward +=reward
+#     print(f'Total Reward for episode {episode} is {total_reward}')
+#     time.sleep(2)
